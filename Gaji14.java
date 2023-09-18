e7a1fcb77abee53b6502d3e8e816133036680b87
Gerly Vaeyungfan <gherly.ylrehg@gmail.com>
2023-09-19 06:58:34 +0700

    modifikasi pseudocode Masukkan gaji dan potongan

--- Gaji14.java
+++ Gaji14.java
@@ -4,7 +4,11 @@ public class Gaji14 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int jmlMasuk, jmlTdkMasuk, totalGaji;
-        int gaji = 400000, ptgnGaji = 25000;
+        int gaji, ptgnGaji;
+        System.out.print("Masukan Jumlah Gaji Anda : : ");
+        gaji = input.nextInt();
+        System.out.print("Masukan Jumlah Potongan Gaji Anda : ");
+        ptgnGaji = input.nextInt();
         System.out.print("Masukan Jumlah Hari Masuk Kerja Anda : ");
         jmlMasuk = input.nextInt();
         System.out.print("Masukan Jumlah Hari Tidak Masuk Kerja Anda : ");
