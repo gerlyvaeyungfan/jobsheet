e3bfc3414bc55f967f6fb3e34c36de3ae6bc5a91
Gerly Vaeyungfan <gherly.ylrehg@gmail.com>
2023-10-01 10:06:19 +0700

    PERCOBAAN 1 JOBSHEET 5

---
+++ PemilihanPercobaan113.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class PemilihanPercobaan113 {
+    public static void main(String[] args) {
+        Scanner input13 = new Scanner(System.in);
+
+        System.out.print("Masukkan angka : ");
+        int angka = input13.nextInt();
+
+        if (angka == 1)
+            System.out.println("Angka " + angka + " bilangan ganjil");
+        else if (angka == 2)
+            System.out.println("Angka " + angka + " bilangan genap");
+    }
+}
