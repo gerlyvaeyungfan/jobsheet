7d822462a0bbe4b838849ac9bc11136302f2b10d
Gerly Vaeyungfan <gherly.ylrehg@gmail.com>
2023-10-01 10:25:56 +0700

    JOBSHEET 5 - PERCOBAAN 2

---
+++ PemilihanPercobaan213.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PemilihanPercobaan213 {
+    public static void main(String[] args) {
+        Scanner input13 = new Scanner(System.in);
+
+        System.out.print("Nilai UAS : ");
+        float uas = input13.nextFloat();
+        System.out.print("Nilai UTS : ");
+        float uts = input13.nextFloat();
+        System.out.print("Nilai UAS : ");
+        float kuis = input13.nextFloat();
+        System.out.print("Nilai UAS : ");
+        float tugas = input13.nextFloat();
+
+        float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
+
+        String message = total < 65 ? "Remidi " : "Tidak Remidi";
+
+        System.out.println("Nilai akhir = " + total + " sehingga " + message);
+    }
+}
