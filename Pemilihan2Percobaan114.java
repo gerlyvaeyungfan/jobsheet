e9d0c3a3fa5c878e392aab849eec796516a2afcc
Gerly Vaeyungfan <gherly.ylrehg@gmail.com>
2023-10-03 09:15:48 +0700

    JOBSHEET 6 - PERCOBAAN 1

---
+++ Pemilihan2Percobaan114.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan114 {
+    public static void main(String[] args) {
+        Scanner input14 = new Scanner(System.in);
+        int tahun;
+
+        System.out.print("Masukkan Tahun : ");
+        tahun = input14.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0) {
+                System.out.println("Tahun Kabisat");
+            } else if ((tahun % 400) == 0) {
+                System.out.println("Tahun Kabisat");
+            } else {
+                System.out.println("Bukan Tahun Kabisat");
+            }
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        }
+    }
+}
