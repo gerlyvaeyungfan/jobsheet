7ce5beee2d775e12379c25d96755755523c8c75c
Gerly Vaeyungfan <gherly.ylrehg@gmail.com>
2023-10-05 09:19:14 +0700

    JOBSHEET 3 - PERCOBAAN 1

---
+++ ContohVariabel14.java
@@ -0,0 +1,16 @@
+public class ContohVariabel14 {
+
+    public static void main(String[] args) {
+        String hb = "Bermain Piano";
+        boolean isPd = true;
+        char jk = 'L';
+        byte umur = 17;
+        double ipk = 3.95, tinggi = 1.67;
+        System.out.println(hb);
+        System.out.println("Apakah pandai?");
+        System.out.println(isPd);
+        System.out.println("Jenis kelamin: " + jk);
+        System.out.println("Umurku saat ini: " + umur);
+        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", ipk, tinggi));
+    }
+}
