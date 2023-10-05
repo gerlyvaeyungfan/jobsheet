5a01d9c91b790a32b23256983f2de05273fe6b9e
Gerly Vaeyungfan <gherly.ylrehg@gmail.com>
2023-10-05 09:19:52 +0700

    JOBSHEET 3 - PERCOBAAN 2

---
+++ ContohTipeData14.java
@@ -0,0 +1,19 @@
+public class ContohTipeData14 {
+    public static void main(String[] args) {
+        char golDrh = 'A';
+        int jarak = 130;
+        short jmlPdk = 1025;
+        float suhu = 60.50f;
+        double berat = 0.5467812345;
+        long saldo = 150000000;
+        int angkaDsml = 0x10;
+
+        System.out.println("Golongan darah\t\t\t\t: " + golDrh);
+        System.out.println("Jarak\t\t\t\t\t: " + jarak);
+        System.out.println("Jumlah penduduk dalam satu dusun\t: " + jmlPdk);
+        System.out.println("Suhu\t\t\t\t\t: " + suhu);
+        System.out.println("Berat\t\t\t\t\t: " + berat);
+        System.out.println("Saldo\t\t\t\t\t: " + saldo);
+        System.out.println("Angka desimal\t\t\t\t: " + angkaDsml);
+    }
+}
