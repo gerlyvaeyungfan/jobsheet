f46fd9fc7a511b9f5809310349cc0e4b0d225cb5
Gerly Vaeyungfan <gherly.ylrehg@gmail.com>
2023-10-05 09:13:26 +0700

    JOBSHEET 1 - TUGAS 1

---
+++ MyFirstJava14.java
@@ -0,0 +1,12 @@
+public class MyFirstJava14 {
+    public static void main(String[] args) {
+        System.out.println("Nama saya Gerly Vaeyungfan");
+        System.out.println("Saya adalah mahasiswa baru jurusan Teknologi Informasi-POLINEMA");
+        System.out.println("\n***** IDENTITAS DIRI *****");
+        System.out.println("Nama      : Gerly Vaeyungfan");
+        System.out.println("Absen     : 14");
+        System.out.println("Kelas     : 1A");
+        System.out.println("Prodi     : D-IV Sistem Informasi Bisnis");
+        System.out.println("Jurusan   : Teknologi Informasi");
+    }
+}
