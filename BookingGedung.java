04fea4ceb51ee68c815eb14a30f14c3d2c4b9322
Gerly Vaeyungfan <gherly.ylrehg@gmail.com>
2023-10-05 09:24:02 +0700

    JOBSHEET 3 - TUGAS 1

---
+++ BookingGedung.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BookingGedung {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = "gd";
+        int tgl, bln, thn, tm;
+
+        System.out.println("Masukkan Nama gedung yang akan dipesan: ");
+        gd = sc.nextString();
+        System.out.println("Masukkan nama Anda: ");
+        name = sc.nextString();
+
+        System.out.println("Nota Anda : ");
+
+    }
+}
