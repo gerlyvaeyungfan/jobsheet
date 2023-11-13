de21702fa59b7842041cdc8034af068d65f7783d
gerlyvaeyungfan <gherly.ylrehg@gmail.com>
2023-11-13 08:37:38 +0700

    JOBSHEET

--- JOBSHEET 11/ProyekJobsheet11.java
+++ JOBSHEET 11/ProyekJobsheet11.java
@@ -2,48 +2,110 @@ import java.util.Scanner;
 public class ProyekJobsheet11 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int tm, gd, menuGd1, menuGd2, menuGd3, menuGd4;
+        int stepLogin;
+        String inputUserAdmin, inputPwAdmin, inputUserMember, inputPwMember;
+        int stepGd1, stepGd2, stepGd3, stepGd4;
         String kodeVeriv[] = {"01234", "12345", "23456", "34567", "45678", "56789", "67890"};
         String inputKodeMenu1, inputKodeMenu2;
-        int subMenu1gd1, subMenu2gd1, subMenu3gd1, subMenu4gd1;
-        int subMenu1gd2, subMenu2gd2, subMenu3gd2, subMenu4gd2;
-        int subMenu1gd3, subMenu2gd3, subMenu3gd3, subMenu4gd3;
-        int subMenu1gd4, subMenu2gd4, subMenu3gd4, subMenu4gd4;
-        int mtdBayarGd1, mtdBayarGd2, mtdBayarGd3, mtdBayarGd4;
-        int stepGd1, stepGd2, stepGd3, stepGd4;
+        int tm, gd, menuGd1, menuGd2, menuGd3, menuGd4;
+        String name, tgl, verivikasi;
         long tarif;
-        long hargaPil1gd1, hargaPil2gd1, hargaPil3gd1, hargaPil4gd1;
-        long hargaPil1gd2, hargaPil2gd2, hargaPil3gd2, hargaPil4gd2;
-        long hargaPil1gd3, hargaPil2gd3, hargaPil3gd3, hargaPil4gd3;
-        long hargaPil1gd4, hargaPil2gd4, hargaPil3gd4, hargaPil4gd4;
-        String name, tgl, Verivikasi;
-        String userAdmin[] = {"admin1", "admin2", "admin3", "admin4"};
-        String pwAdmin[]={"01", "02", "03", "04", "05"};
-        String brgTersediaGd1[][]={
-            {"Undangan Nikah", "Rp.5000/pcs"},
-            {"Catering", "Rp.30000/pcs"},
-            {"Souvenir", "Rp.25000/pcs"},
-            {"Dekorasi", "Rp.2500000"},
-        };
-        String brgTersediaGd2[][]={
-            {}
-        };
-        String brgTersediaGd3[][]={
-            {}
-        };
-        String brgTersediaGd4[][]={
-            {}
-        };
+        String[] newUser = new String[100];
+        String[] newPw = new String[100];
+        int userCount = 0;
+        userCount++;
         String namaGd[][] = { 
             {"Gedung Pernikahan",  "900 Orang", "Rp. 90000"},
             {"Gedung Olahraga", "250 Orang", "Rp. 25000"},
             {"Gedung Kesenian", "100 Orang", "Rp. 10000"},
             {"Gedung Auditorium", "500 Orang", "Rp. 50000"}
         };
+        String akun;
+        do {
+            System.out.print("Apakah Anda sudah punya akun? (y/t)");
+            akun = input.next();
+
+            if (akun.equals("t")){
+                System.out.print("Masukkan username baru: ");
+                String inputUserBaru = input.next();
+                System.out.print("Masukkan password baru: ");
+                String inputPwBaru = input.next();
+
+                newUser[userCount] = inputUserBaru;
+                newPw[userCount] = inputPwBaru;
+                userCount++;
+                System.out.println("User baru berhasil terdaftar!");
+            } else if (akun.equals("y")) {
+
+            do {
+            System.out.println("*______________________________________________*");
+            System.out.println("|  Selamat Datang di Website Booking Gedung:)  |");
+            System.out.println("|----------------------------------------------|");
+            System.out.println("|        Silahkan pilih halaman login!         |");
+            System.out.println("|   1. Login Admin                             |");
+            System.out.println("|   2. Login Member                            |");
+            System.out.println("|   3. Keluar                                  |");
+            System.out.println("|______________________________________________|");
+
+            System.out.print("Masukkan Pilihan Anda : ");
+            stepLogin = input.nextInt();
+                if (stepLogin < 1 || stepLogin > 4) {
+                    System.out.println("Mohon masukkan nomor antara 1 dan 4.");
+                }
+        } while (stepLogin < 1 || stepLogin > 4);
+    if (stepLogin==1) {
+        String[][] users = {
+            {"admin1", "admin111"},
+            {"admin2", "admin222"}
+        };
+        boolean userAdminValid;
+        do{
+        System.out.print("Masukkan username: ");
+        inputUserAdmin = input.next();
+        System.out.print("Masukkan password: ");
+        inputPwAdmin = input.next();
+        userAdminValid=false;
+        for (String[] user : users) {
+            if (user[0].equals(inputUserAdmin) && user[1].equals(inputPwAdmin)) {
+                userAdminValid = true;
+                break;
+            }
+        }
+        if (userAdminValid) {
+            System.out.println("Login berhasil!");
+            System.out.println("Selamat datang, Admin!");
+        } else {
+            System.out.println("Login gagal. Username atau password salah. Silakan coba lagi.");
+        }
+        } while (!userAdminValid);
+
+    } else if (stepLogin==2){
+
+        boolean userMemberValid;
+        do{
+        System.out.print("Masukkan username: ");
+        inputUserMember = input.next();
+        System.out.print("Masukkan password: ");
+        inputPwMember = input.next();
+        userMemberValid=false;
+        for (int i = 0; i < userCount; i++) {
+            if (inputUserMember.equals(newUser[i]) && inputPwMember.equals(newPw[i])) {
+                userMemberValid = true;
+                break;
+            }
+        }
+        if (userMemberValid) {
+            System.out.println("Login berhasil!");
+            System.out.println("Selamat datang, Member!");
+        } else {
+            System.out.println("Login gagal. Username atau password salah. Silakan coba lagi.");
+        }
+        } while (!userMemberValid);
+        
         while (true) {
             System.out.print("\nApakah Anda ingin memesan gedung (y/t)? ");
-            Verivikasi = input.next();
-            if (Verivikasi.equalsIgnoreCase("y")) {
+            verivikasi = input.next();
+            if (verivikasi.equalsIgnoreCase("y")) {
                 System.out.println("GEDUNG YANG TERSEDIA!");
                 System.out.printf("%-5s%-20s\n", "No.", "Nama Gedung\t  Maks Tamu\t  Tarif");
                 for (int i = 0; i < namaGd.length; i++) {
@@ -90,6 +152,7 @@ public class ProyekJobsheet11 {
                                 System.out.println("Mohon masukkan nomor gedung antara 1 dan 2.");
                             }
                         } while (stepGd1 < 1 || stepGd1 > 2);
+                        int mtdBayarGd1;
                         switch (stepGd1) {
                             case 1:
                                 System.out.println("1. Bayar lewat BRI/BNI/BCA di no.rek 2341760195 a.n Gerly Vaeyungfan");
@@ -138,6 +201,12 @@ public class ProyekJobsheet11 {
                                 }
                                 break;
                             case 2:
+                                String brgTersediaGd1[][]={
+                                    {"Undangan Nikah", "Rp.5000/pcs"},
+                                    {"Catering", "Rp.30000/pcs"},
+                                    {"Souvenir", "Rp.25000/pcs"},
+                                    {"Dekorasi", "Rp.2500000"},
+                                };
                                 for (int i = 0; i < brgTersediaGd1.length; i++) {
                                     System.out.print((i + 1) + ". ");
                                     for (int j=0; j<brgTersediaGd1[i].length; j++){
@@ -152,6 +221,8 @@ public class ProyekJobsheet11 {
                                         System.out.println("Mohon masukkan angka antara 1 dan 4.");
                                     }
                                 } while (menuGd1 < 1 || menuGd1 > 4);
+                                int subMenu1gd1, subMenu2gd1, subMenu3gd1, subMenu4gd1;
+                                long hargaPil1gd1, hargaPil2gd1, hargaPil3gd1, hargaPil4gd1;
                                 switch (menuGd1) {
                                     case 1:
                                         System.out.print("Jumlah undangan yang ingin dipesan : ");
@@ -198,6 +269,7 @@ public class ProyekJobsheet11 {
                                     System.out.println("Mohon masukkan angka antara 1 dan 2.");
                                 }
                             } while (stepGd2 < 1 || stepGd2 > 2);
+                        int mtdBayarGd2;
                         switch (stepGd2) {
                             case 1:
                                 System.out.println("1. Bayar lewat BRI/BNI/BCA di no.rek 2341760195 a.n Gerly Vaeyungfan");
@@ -246,6 +318,12 @@ public class ProyekJobsheet11 {
                                 }
                                 break;
                             case 2:
+                                String brgTersediaGd2[][]={
+                                    {},
+                                    {},
+                                    {},
+                                    {},
+                                    };
                                 for (int i = 0; i < brgTersediaGd2.length; i++) {
                                     System.out.print((i + 1) + ". ");
                                     for (int j=0; j<brgTersediaGd2[i].length; j++){
@@ -255,6 +333,8 @@ public class ProyekJobsheet11 {
                                 }
                                 System.out.print("Masukkan Pilihan Anda : ");
                                 menuGd2=input.nextInt();
+                                int subMenu1gd2, subMenu2gd2, subMenu3gd2, subMenu4gd2;
+                                long hargaPil1gd2, hargaPil2gd2, hargaPil3gd2, hargaPil4gd2;
                                 switch (menuGd2) {
                                     case 1:
                                         System.out.print("Jumlah undangan yang ingin dipesan : ");
@@ -303,6 +383,7 @@ public class ProyekJobsheet11 {
                                 System.out.println("Mohon masukkan angka antara 1 dan 2.");
                             }
                         } while (stepGd3 < 1 || stepGd3 > 2);
+                        int mtdBayarGd3;
                         switch (stepGd3) {
                             case 1:
                                 System.out.println("1. Bayar lewat BRI/BNI/BCA di no.rek 2341760195 a.n Gerly Vaeyungfan");
@@ -351,6 +432,12 @@ public class ProyekJobsheet11 {
                                 }
                                 break;
                             case 2:
+                                String brgTersediaGd3[][]={
+                                    {},
+                                    {},
+                                    {},
+                                    {},
+                                };
                                 for (int i = 0; i < brgTersediaGd3.length; i++) {
                                     System.out.print((i + 1) + ". ");
                                     for (int j=0; j<brgTersediaGd3[i].length; j++){
@@ -360,6 +447,8 @@ public class ProyekJobsheet11 {
                                 }
                                 System.out.print("Masukkan Pilihan Anda : ");
                                 menuGd3=input.nextInt();
+                                int subMenu1gd3, subMenu2gd3, subMenu3gd3, subMenu4gd3;
+                                long hargaPil1gd3, hargaPil2gd3, hargaPil3gd3, hargaPil4gd3;
                                 switch (menuGd3) {
                                     case 1:
                                         System.out.print("Jumlah undangan yang ingin dipesan : ");
@@ -408,6 +497,7 @@ public class ProyekJobsheet11 {
                                     System.out.println("Mohon masukkan angka antara 1 dan 2.");
                                 }
                             } while (stepGd4 < 1 || stepGd4 > 2);
+                            int mtdBayarGd4;
                         switch (stepGd4) {
                             case 1:
                                 System.out.println("1. Bayar lewat BRI/BNI/BCA di no.rek 2341760195 a.n Gerly Vaeyungfan");
@@ -456,6 +546,12 @@ public class ProyekJobsheet11 {
                                 }
                                 break;
                             case 2:
+                                String brgTersediaGd4[][]={
+                                    {},
+                                    {},
+                                    {},
+                                    {},
+                                };
                                 for (int i = 0; i < brgTersediaGd4.length; i++) {
                                     System.out.print((i + 1) + ". ");
                                     for (int j=0; j<brgTersediaGd4[i].length; j++){
@@ -465,6 +561,8 @@ public class ProyekJobsheet11 {
                                 }
                                 System.out.print("Masukkan Pilihan Anda : ");
                                 menuGd4=input.nextInt();
+                                int subMenu1gd4, subMenu2gd4, subMenu3gd4, subMenu4gd4;
+                                long hargaPil1gd4, hargaPil2gd4, hargaPil3gd4, hargaPil4gd4;
                                 switch (menuGd4) {
                                     case 1:
                                         System.out.print("Jumlah undangan yang ingin dipesan : ");
@@ -488,13 +586,22 @@ public class ProyekJobsheet11 {
                     default:
                         break;
                 }
-            } else if (Verivikasi.equalsIgnoreCase("t")) {
+            } else if (verivikasi.equalsIgnoreCase("t")) {
                 System.out.print("Terima kasih! Selamat berjumpa kembali:)");
                 System.exit(0);
             } else {
                 System.out.println("Maaf, pilihan anda salah! Silakan ulangi.");
             }
+            break;
         }
+    } else if (stepLogin==3){
+    System.out.println("Terima kasih.");
+    System.exit(0);
+    
     }
+    } else if (!akun.equals("y") || !akun.equals("t")){
+        System.out.println("Pilihan tidak tersedia");
+    }
+    } while (!akun.equals("y") || !akun.equals("t"));
 }
-
+}
