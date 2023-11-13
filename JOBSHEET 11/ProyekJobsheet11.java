a20ad579f9d9b3e7bbcd007b2a88484bfbbb163c
gerlyvaeyungfan <gherly.ylrehg@gmail.com>
2023-11-13 09:58:10 +0700

    JOBSHEET

--- JOBSHEET 11/ProyekJobsheet11.java
+++ JOBSHEET 11/ProyekJobsheet11.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 
-import javax.sound.midi.Soundbank;
 public class ProyekJobsheet11 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
