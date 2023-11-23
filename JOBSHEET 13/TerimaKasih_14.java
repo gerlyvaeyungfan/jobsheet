9e30a1aa9c8a15b2108fcb89c9dc01aed3a59a4b
gerlyvaeyungfan <gherly.ylrehg@gmail.com>
2023-11-24 05:45:18 +0700

    JOBSHEET

--- JOBSHEET 13/TerimaKasih_14.java
+++ JOBSHEET 13/TerimaKasih_14.java
@@ -3,7 +3,14 @@ public class TerimaKasih_14{
         System.out.println("Thank you for being the best teacher inthe world.\n"+
         "You inspired in me a love for learning and made me feel like I could ask you anything.");
     }
+
+    public static void UcapanTambahan(String ucapan) {
+        System.out.println(ucapan);
+    }
     public static void main(String[] args){
         UcapanTerimaKasih();
+        String ucapan = "Terima Kasih Pak.. Bu.. Semoga Sehat Selalu";
+        UcapanTambahan(ucapan);
     }
 }
+
