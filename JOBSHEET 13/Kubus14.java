41225cb762582778437e8d8823931dbf18ff9800
gerlyvaeyungfan <gherly.ylrehg@gmail.com>
2023-11-25 05:05:09 +0700

    JOBSHEET

--- JOBSHEET 13/Kubus14.java
+++ JOBSHEET 13/Kubus14.java
@@ -2,88 +2,30 @@ import java.util.Scanner;
 
 public class Kubus14 {
 
-    static int jumlahMahasiswa = 5;
-    static int jumlahMinggu = 7;
-    static int nilaiTertinggi = 0;
-    static int mingguNilaiTertinggi = 0;
-    static String nama;
-
-    static int[][] nilaiMahasiswa = new int[jumlahMahasiswa][jumlahMinggu];
-
-    static void main(String[] args) {
-        namaMahasiswa(nama);
-        jumlahNilaiMahasiswa();
-        tampilkanNilaiMahasiswa();
-        lacakNilaiTertinggi();
-        tampilMahasiswaNilaiTertimggi();
-    }
-    
-    
-    static void namaMahasiswa(String nama){
-        Scanner input = new Scanner(System.in);
-        System.out.print("Masukkan Nama Mahaiswa : ");
-        nama = input.next();
-    }
-    
-    static void jumlahNilaiMahasiswa() {
-        Scanner input = new Scanner(System.in);
-
-        for (int i = 0; i < jumlahMahasiswa; i++) {
-            System.out.println("Masukkan data nilai mahasiswa ke-" + (i + 1));
-            for (int j = 0; j < jumlahMinggu; j++) {
-                System.out.print("Minggu ke-" + (j + 1) + ": ");
-                nilaiMahasiswa[i][j] = input.nextInt();
-            }
-        }
+    // Fungsi untuk menghitung volume kubus
+    public static double hitungVolumeKubus(double sisi) {
+        double volume = Math.pow(sisi, 3);
+        return volume;
     }
 
-    static void tampilkanNilaiMahasiswa() {
-        System.out.println("._______________________________________________________________________________________________________________________________________.");
-        System.out.println("|                                                                                                                                       |");
-        System.out.println("|                                                                 DATA NILAI MAHASISWA                                                  |");
-        System.out.println("|                                                                                                                                       |");
-        System.out.println("|=======================================================================================================================================|");
-        System.out.println("|                |  Minggu ke-1   |  Minggu ke-2   |  Minggu ke-3   |  Minggu ke-4   |  Minggu ke-5   |  Minggu ke-6   |  Minggu ke-7   |");
-        System.out.println("|================|================|================|================|================|================|================|================|");
-        for (int i = 0; i < jumlahMahasiswa; i++) {
-            namaMahasiswa(nama);
-            for (int j = 0; j < jumlahMinggu; j++) {
-                System.out.format("%-15d", nilaiMahasiswa[i][j]);
-                if (j < jumlahMinggu) {
-                    System.out.print("| ");
-                }
-            }
-            System.out.println();
-        }
-        System.out.println("|________________|________________|________________|________________|________________|________________|________________|________________|");
+    // Fungsi untuk menghitung luas permukaan kubus
+    public static double hitungLuasPermukaanKubus(double sisi) {
+        double luasPermukaan = 6 * Math.pow(sisi, 2);
+        return luasPermukaan;
     }
 
-    static void lacakNilaiTertinggi() {
-        
-        for (int i = 0; i < jumlahMahasiswa; i++) {
-            for (int j = 0; j < jumlahMinggu; j++) {
-                if (nilaiMahasiswa[i][j] > nilaiTertinggi) {
-                    nilaiTertinggi = nilaiMahasiswa[i][j];
-                    mingguNilaiTertinggi = j + 1;
-                }
-            }
-        }
-        System.out.println("-> Nilai tertinggi adalah " + nilaiTertinggi + " pada minggu ke-" + mingguNilaiTertinggi);
-        System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------");
-    }
+    public static void main(String[] args) {
+        // Membaca panjang sisi kubus dari pengguna
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan panjang sisi kubus: ");
+        double sisiKubus = scanner.nextDouble();
 
-    static void tampilMahasiswaNilaiTertimggi() {
-        int nilaiTertinggi = 0;
-        int mahasiswaDenganNilaiTertinggi = 0;
-        for (int i = 0; i < jumlahMahasiswa; i++) {
-            for (int j = 0; j < jumlahMinggu; j++) {
-                if (nilaiMahasiswa[i][j] > nilaiTertinggi) {
-                    nilaiTertinggi = nilaiMahasiswa[i][j];
-                    mahasiswaDenganNilaiTertinggi = i;
-                }
-            }
-        }
-        System.out.println("-> Mahasiswa dengan nilai tertinggi adalah mahasiswa ke-" + (mahasiswaDenganNilaiTertinggi + 1) + " dengan nilai " + nilaiTertinggi + " pada minggu ke-" + mingguNilaiTertinggi);
-        System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------");
+        // Menghitung volume kubus
+        double volume = hitungVolumeKubus(sisiKubus);
+        System.out.println("Volume kubus dengan sisi " + sisiKubus + " adalah: " + volume);
+
+        // Menghitung luas permukaan kubus
+        double luasPermukaan = hitungLuasPermukaanKubus(sisiKubus);
+        System.out.println("Luas permukaan kubus dengan sisi " + sisiKubus + " adalah: " + luasPermukaan);
     }
 }
