88df9ab3d9f34a7bd04d26459342114ed92527c1
gerlyvaeyungfan <gherly.ylrehg@gmail.com>
2023-11-28 12:22:46 +0700

    JOBSHEET

--- Rekursif/Percobaan2.java
+++ Rekursif/Percobaan2.java
@@ -8,12 +8,26 @@ public class Percobaan2 {
             return (x * hitungPangkat(x, y-1));
         }
     }
+
+    static void cetakDeretPangkat(int x, int y) {
+        System.out.print("hitungPangkat(" + x + "," + y + ") dicetak ");
+        for (int i = 1; i <= y; i++) {
+            System.out.print(x);
+            if (i < y) {
+                System.out.print("x");
+            } else {
+                System.out.print("x1 = " + hitungPangkat(x, y));
+            }
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Bilangan yang dihitung : ");
         int bilangan = sc.nextInt();
         System.out.print("Pangkat : ");
         int pangkat = sc.nextInt();
+        cetakDeretPangkat(bilangan, pangkat);
         System.out.println("Hasil dari "+bilangan+" pangkat "+pangkat+" adalah "+hitungPangkat(bilangan, pangkat));
     }
 }
